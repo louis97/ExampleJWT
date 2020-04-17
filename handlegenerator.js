1e246a1c12d2282c33fe5b73d7cc558754d850b7
Louis Gualtero <lf.gualtero@uniandes.edu.co>
2020-04-17 09:33:58 -0500

    commit2

--- handlegenerator.js
+++ handlegenerator.js
@@ -1,80 +1,73 @@
-let jwt = require( 'jsonwebtoken' );
-let config = require( './config' );
+let jwt = require('jsonwebtoken');
+let config = require('./config');
 var express = require('express');
-var router = express.Router();
 const Mongolib = require("./db/Mongolib.js");
 
 // Clase encargada de la creación del token
 class HandlerGenerator {
 
-  login( req, res ) {
-    
-    // Extrae el usuario y la contraseña especificados en el cuerpo de la solicitud
-    let username = req.body.username;
-    let password = req.body.password;
+    login(req, res) {
 
-    // Este usuario y contraseña, en un ambiente real, deben ser traidos de la BD
-    let mockedUsername = 'admin';
-    let mockedPassword = 'password';
+        // Extrae el usuario y la contraseña especificados en el cuerpo de la solicitud
+        let username = req.body.username;
+        let password = req.body.password;
 
-    router.get('/', function (req, res, next) {
-        Mongolib.getDatabase(db => {
-            Mongolib.findDocuments(db, docs => {
-                console.log(docs);
-            })
-        })
-    });
+        // Este usuario y contraseña, en un ambiente real, deben ser traidos de la BD
+        let mockedUsername = 'admin';
+        let mockedPassword = 'password';
 
-    // Si se especifico un usuario y contraseña, proceda con la validación
-    // de lo contrario, un mensaje de error es retornado
-    if( username && password ) {
 
-      // Si los usuarios y las contraseñas coinciden, proceda con la generación del token
-      // de lo contrario, un mensaje de error es retornado
-      if( username === mockedUsername && password === mockedPassword ) {
-        
-        // Se genera un nuevo token para el nombre de usuario el cuál expira en 24 horas
-        let token = jwt.sign( { username: username },
-          config.secret, { expiresIn: '24h' } );
-        
-        // Retorna el token el cuál debe ser usado durante las siguientes solicitudes
-        res.json( {
-          success: true,
-          message: 'Authentication successful!',
-          token: token
-        } );
+        // Si se especifico un usuario y contraseña, proceda con la validación
+        // de lo contrario, un mensaje de error es retornado
+        if (username && password) {
 
-      } else {
-        
-        // El error 403 corresponde a Forbidden (Prohibido) de acuerdo al estándar HTTP
-        res.send( 403 ).json( {
-          success: false,
-          message: 'Incorrect username or password'
-        } );
+            // Si los usuarios y las contraseñas coinciden, proceda con la generación del token
+            // de lo contrario, un mensaje de error es retornado
+            if (username === mockedUsername && password === mockedPassword) {
 
-      }
+                // Se genera un nuevo token para el nombre de usuario el cuál expira en 24 horas
+                let token = jwt.sign({ username: username },
+                    config.secret, { expiresIn: '24h' });
 
-    } else {
+                // Retorna el token el cuál debe ser usado durante las siguientes solicitudes
+                res.json({
+                    success: true,
+                    message: 'Authentication successful!',
+                    token: token
+                });
 
-      // El error 400 corresponde a Bad Request de acuerdo al estándar HTTP
-      res.send( 400 ).json( {
-        success: false,
-        message: 'Authentication failed! Please check the request'
-      } );
+            } else {
+
+                // El error 403 corresponde a Forbidden (Prohibido) de acuerdo al estándar HTTP
+                res.send(403).json({
+                    success: false,
+                    message: 'Incorrect username or password'
+                });
+
+            }
+
+        } else {
+
+            // El error 400 corresponde a Bad Request de acuerdo al estándar HTTP
+            res.send(400).json({
+                success: false,
+                message: 'Authentication failed! Please check the request'
+            });
+
+        }
 
     }
 
-  }
-
-  index( req, res ) {
-    
-    // Retorna una respuesta exitosa con previa validación del token
-    res.json( {
-      success: true,
-      message: 'Index page'
-    } );
-
-  }
+    index(req, res) {
+        Mongolib.getDatabase(db => {
+            Mongolib.findDocuments(db, docs => {
+                res.json({
+                    success: true,
+                    message: docs
+                });
+            })
+        })
+    }
 }
 
 module.exports = HandlerGenerator;
